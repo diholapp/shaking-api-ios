72a50bb14ae08108915fa78f347e59b7dff5fefb
Juanse Brito <juansebr98@hotmail.com>
2019-06-13 12:57:20 +0200

    Initial commit

---
+++ DiHolaShakingAPI/DiHolaShakingAPI.h
@@ -0,0 +1,19 @@
+//
+//  DiHolaShakingAPI.h
+//  DiHolaShakingAPI
+//
+//  Created by Juanse Brito on 12/06/2019.
+//  Copyright © 2019 DiHola S.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DiHolaShakingAPI.
+FOUNDATION_EXPORT double DiHolaShakingAPIVersionNumber;
+
+//! Project version string for DiHolaShakingAPI.
+FOUNDATION_EXPORT const unsigned char DiHolaShakingAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DiHolaShakingAPI/PublicHeader.h>
+
+
